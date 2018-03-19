95b71fcb3a4ba280bfd7487a9b870e02a1af8a25
Jaeyeong Huh <jyhuh@dogtra.com>
2018-03-19 18:32:02 +0900

    [ Network ] Make 'interfaces' file and change /etc/network/interfaces.

--- cp_to_admin.sh
+++ cp_to_admin.sh
@@ -5,6 +5,7 @@ LOCAL_CONF_PATH=$LOCAL_ADMIN_PATH/config
 CONF_BACKUP=/root/.config-backup
 
 UDEV_NET_FILE=/etc/udev/rules.d/70-persistent-net.rules
+NET_INTERFACE_FILE=/etc/network/interfaces
 
 if [ -z "`which sudo`" ] ; then
 	apt-get update
@@ -15,11 +16,43 @@ if [ ! -e "$CONF_BACKUP" ] ; then
 	sudo mkdir -p $CONF_BACKUP
 fi
 
+cat << EOF > ./interfaces
+# interfaces(5) file used by ifup(8) and ifdown(8)
+auto lo
+iface lo inet loopback
+
+# The primary network interface
+auto eth0
+iface eth0 inet dhcp
+
+#iface eth0 inet static
+#	address 112.151.139.189
+#	netmask 255.255.255.240
+#	gateway 112.151.139.177
+#dns-nameservers 203.248.252.2 164.124.101.2
+#dns-nameservers 168.126.63.1 168.126.63.2 8.8.8.8
+
+#auto eth1
+#iface eth1 inet static
+#	address 192.168.5.189
+#	netmask 255.255.255.0
+#	gateway 192.168.5.1
+
+#pre-up iptables-restore < /etc/iptables.rules
+#post-down iptables-save > /etc/iptables.rules
+EOF
+
 if [ -z "`ifconfig -a | grep eth`" ] ; then
-	sudo sed -i "s/CMDLINE_LINUX=\"\"/CMDLINE_LINUX=\"net.ifnames=0 biosname=0\"/" /etc/default/grub
+	sudo sed -i "s/CMDLINE_LINUX=\"\"/CMDLINE_LINUX=\"net.ifnames=0 biosdevname=0\"/" /etc/default/grub
 	sudo update-grub2
 	if [ -e "$UDEV_NET_FILE" ] ; then
 		sudo mv $UDEV_NET_FILE $CONF_BACKUP
+	fi
+	if [ -e "$NET_INTERFACE_FILE" ] ; then
+		sudo mv $NET_INTERFACE_FILE $CONF_BACKUP
+		sudo mv ./interfaces $NET_INTERFACE_FILE
+	else
+		rm ./interfaces
 	fi
 fi
 
