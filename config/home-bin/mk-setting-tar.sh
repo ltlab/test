9d5cbe8563f087658d0a1be9837c387686da31ad
Jaeyeong Huh <jyhuh@dogtra.com>
2018-03-21 10:47:39 +0900

    modify mk-setting-tar.sh.

--- config/home-bin/mk-setting-tar.sh
+++ config/home-bin/mk-setting-tar.sh
@@ -7,19 +7,15 @@ TIME_TAG1=$(date +"_%T")
 
 rm -f elechole-setting_*
 #touch elechole-setting_$DATE_TAG$TIME_TAG.tar.gz
-cp -a ~/bin/* ~/setting/usr_setting/script/
-cp ~/.vimrc ~/setting/usr_setting/elechole-config/
-cp ~/.bashrc ~/setting/usr_setting/elechole-config/
-cp ~/.profile ~/setting/usr_setting/elechole-config/
-#cp ~/.bash_profile ~/setting/usr_setting/elechole-config/
-cp ~/.gitconfig ~/setting/usr_setting/elechole-config/
 
 # Remove ELF Files
 find ./setting/test_src/ -exec file {} \; | grep -i elf | sed 's/://' | awk '{print $1}' | xargs rm -vf
 
 #tar -zcvf elechole-setting_$DATE_TAG.tar.gz ./setting/ ./.vim/ ./bin/ .vimrc .bashrc .profile .bash_profile .gitconfig .subversion
-tar -zcvf elechole-setting_$DATE_TAG.tar.gz ./setting/ ./.vim/ ./bin/ .vimrc .bashrc .profile .gitconfig .subversion .vim_backup/ .screenrc .tmux.conf .ctags \
-	.config/powerline \
+#tar -zcvf elechole-setting_$DATE_TAG.tar.gz ./setting/ ./.vim/ ./bin/ .vimrc .bashrc .profile .gitconfig .subversion .vim_backup/ .screenrc .tmux.conf .ctags \
+#	.config/powerline \
+#	--exclude="*.swp" --exclude="cscope.*" --exclude="tags" --exclude="*~" --exclude="view"
+tar -zcvf elechole-setting_$DATE_TAG.tar.gz ./setting/ .subversion \
 	--exclude="*.swp" --exclude="cscope.*" --exclude="tags" --exclude="*~" --exclude="view"
 
 #./firefox-FEBE/
