00ee144fcec832611abd50c102cfb07fd579fcb8
Jaeyeong Huh <jay.jyhuh@gmail.com>
2020-01-29 16:44:10 +0900

    [ SSH ] Add sshd_config for SSH keepalive.

--- script/base.sh
+++ script/base.sh
@@ -7,6 +7,9 @@ echo "Installing Base Service..."
 
 # SSH
 sudo apt install -q -y openssh-server
+sudo cp -a --backup=numbered /etc/ssh/sshd_config $CONF_BACKUP
+sudo cp -a $CONF_PATH/sshd_config /etc/ssh/sshd_config
+sudo systemctl restart sshd
 
 sudo apt install -q -y make
 sudo apt install -q -y git git-review
