465f57e805d6fa8caf2b81ecb4729aa9464a3679
Jaeyeong Huh <jay.jyhuh@gmail.com>
2020-06-24 14:35:30 +0900

    [ STM32 ] Update stlink-gdbserver.sh
    Signed-off-by: Jaeyeong Huh <jay.jyhuh@gmail.com>

--- config/home-bin/stlink-gdbserver.sh
+++ config/home-bin/stlink-gdbserver.sh
@@ -12,11 +12,12 @@ BIN_PATH="${PLUGIN_PATH}/tools/bin"
 CUBE_PROGRAMMER_PLUGIN_NAME="com.st.stm32cube.ide.mcu.externaltools.cubeprogrammer.linux64"
 CUBE_PROGRAMMER_BIN_PATH="${STM_INSTALL_PATH}/plugins/${CUBE_PROGRAMMER_PLUGIN_NAME}_${PLUGIN_VERSION}/tools/bin"
 
-OPTIONS="-p 61234 -z 61235"		#	gdb port and swo-port
-OPTIONS=${OPTIONS}" -d"				#	swd enable
-OPTIONS=${OPTIONS}" -a 96000000 -b 48"		#	cpu clock and swo clock divider
+OPTIONS="-p 61234 -l 1"		#	gdb port and swo-port
+OPTIONS=${OPTIONS}" -d -z 61235"		#	swd enable
+OPTIONS=${OPTIONS}" -a 96000000 -b 48"	#	cpu clock and swo clock divider
 OPTIONS=${OPTIONS}" -cp ${CUBE_PROGRAMMER_BIN_PATH}"
 OPTIONS=${OPTIONS}" -s -m 0 -k"			#	-s: verification of the flash download -k: init. reset
+#OPTIONS=${OPTIONS}" -e"					#	starts the server in persistent mode
 
 #TOOL_PATH = "/opt/st/stm32cubeide_1.3.0/plugins/com.st.stm32cube.ide.mcu.externaltools.stlink-gdb-server.linux64_1.3.0.202002181050/tools/bin"
 
@@ -24,6 +25,6 @@ echo $OPTIONS
 #exit
 
 cd ${BIN_PATH}
-ls -al
-./ST-LINK_gdbserver ${OPTIONS} -l 4
+#cat config.txt
+./ST-LINK_gdbserver ${OPTIONS}
 
