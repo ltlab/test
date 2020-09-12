75e32bdb85ceb67f6fb54c494647529a5ef0027c
Jaeyeong Huh <jay.jyhuh@gmail.com>
2020-09-12 15:29:11 +0900

    Add `zsh`.
    Signed-off-by: Jaeyeong Huh <jay.jyhuh@gmail.com>

--- script/base.sh
+++ script/base.sh
@@ -20,12 +20,16 @@ sudo cp -a --backup=numbered /etc/ssh/sshd_config $CONF_BACKUP
 sudo cp -a $CONF_PATH/sshd_config /etc/ssh/sshd_config
 sudo systemctl restart sshd
 
+sudo ${APT} ${APT_CACHE_OPTION} install -y -qq zsh
+
 sudo ${APT} ${APT_CACHE_OPTION} install -y -qq make curl cmake
 sudo ${APT} ${APT_CACHE_OPTION} install -y -qq git git-review
 sudo ${APT} ${APT_CACHE_OPTION} install -y -qq gnupg
 
 sudo ${APT} ${APT_CACHE_OPTION} install -y -qq vim ctags cscope shellcheck
 sudo ${APT} ${APT_CACHE_OPTION} install -y -qq silversearcher-ag ack-grep
+
+# TODO: Install ripgrep(rg) and fzf
 
 # Resource Monitoring
 sudo ${APT} ${APT_CACHE_OPTION} install -y -qq tmux sysstat
