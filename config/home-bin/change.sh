d20121f5d18f8b6859a1f87601958c4ac6eebe35
Jaeyeong Huh <jay.jyhuh@gmail.com>
2021-02-16 16:44:23 +0900

    [VIMRD] Update `vimrc` for windows.
    - Change plugin manager `Vundle` to `vim-plug`.
    
    Signed-off-by: Jaeyeong Huh <jay.jyhuh@gmail.com>

--- config/home-bin/change.sh
+++ config/home-bin/change.sh
@@ -1,14 +1,21 @@
-#!/bin/bash
+#!/bin/sh
 
-ORIGINAL_TEXT="org"
-REPLACE_TEXT="replace"
+ORIGINAL_TEXT="$1"
+REPLACE_TEXT="$2"
+DIR="$3"
+
+if [ -z "$DIR" ] ; then
+  echo "PATH not found"
+  exit -1
+fi
 
 #for i in `find . -type f -name Makefile`
-for i in `find . -type f` 
+for i in `find $DIR -type f`
 do
 	#sed -e "s/e-ronix/"\&"/g" $i > $i.old
-	sed -e "s/$ORIGINAL_TEXT/$REPLACE_TEXT/g" $i > $i.old
-	echo -e "sed -e 's/$ORIGINAL_TEXT/$REPLACE_TEXT/g' $i > $i.old"
-	echo -e "$i"
-	mv $i.old $i
+#	sed -e "s/$ORIGINAL_TEXT/$REPLACE_TEXT/g" $i > $i.old
+#	echo -e "sed -e 's/$ORIGINAL_TEXT/$REPLACE_TEXT/g' $i > $i.old"
+#	echo -e "$i"
+#	mv $i.old $i
+	sed -i "s/$ORIGINAL_TEXT/$REPLACE_TEXT/g" $i
 done
